7164d3a20d601a7f2626b2ed24d23191717de27f
Amirtha Varshni M <kit27.am006@gmail.com>
2025-12-17 14:17:25 +0530

    Implement maxSubArray method for maximum sum

--- Arrays/LC053_MaximumSubarray.java
+++ Arrays/LC053_MaximumSubarray.java
@@ -1,1 +1,18 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int curr=nums[0];
+        int ans=nums[0];
+        for(int i=1;i<nums.length;i++) {
+            if (curr<0) {
+                curr=nums[i];   
+            } else {
+                curr+=nums[i];  
+            }
+            if (curr>ans) {
+                ans=curr;
+            }
+        }
+        return ans;
+    }
+}
 
